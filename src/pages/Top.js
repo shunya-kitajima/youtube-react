6321bc62c5a51cf24021f194eb19bd84a769b244
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-06 22:10:27 +0900

    ソースをアップ

---
+++ src/pages/Top.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const Top = () => {
+    return (
+        <Layout>
+            top page
+        </Layout>
+    )
+}
+
+export default Top
