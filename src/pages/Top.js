3a0ff76ef3ef54f7b47b80543b096d8e2bfcaed2
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-16 22:43:24 +0900

    修正

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,12 +1,16 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useContext } from 'react'
 import Layout from '../components/layout/Layout'
-import {fetchPopularData} from '../apis/index';
+import { fetchPopularData } from '../apis/index';
+import { Store } from '../store/index';
 
 const Top = () => {
+    const { globalState, setGlobalState } =  useContext(Store);
     useEffect(() => {
         fetchPopularData().then((resp) => {
             console.log('data', resp);
+            setGlobalState({type: 'SET_POPULAR', payload: {popular: resp.data.items}})
         });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
         <Layout>
