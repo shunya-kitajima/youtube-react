38068deb55e527bca566283e2e4089a3a19932fb
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-16 22:51:19 +0900

    ファイルを追加

---
+++ src/components/VideoGridItem/VideoGridItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Style from './VideoGridItem.module.scss';
+
+const VideoGridItem = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default VideoGridItem
