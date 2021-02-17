4b4da657695fc1ba1f4da9887ba51adbe0f5546c
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-17 21:46:22 +0900

    アイテムを表示するよう修正

--- src/components/VideoGridItem/VideoGridItem.js
+++ src/components/VideoGridItem/VideoGridItem.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Style from './VideoGridItem.module.scss';
+import { Link } from 'react-router-dom';
 
-const VideoGridItem = () => {
+const VideoGridItem = ({id, src, title}) => {
     return (
-        <div>
-            
-        </div>
+        <Link to={{pathname: 'watch', search: `?v=${id}`}} className={Style.item}>
+            <div>
+                <img src={src} alt={title} />
+                <span>{title}</span>
+            </div>
+        </Link>
     )
 }
 
