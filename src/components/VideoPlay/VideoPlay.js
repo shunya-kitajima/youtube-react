03acfebe1c90af2b92856ea5d2eab7408c423667
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-18 21:43:14 +0900

    Video再生を追加

---
+++ src/components/VideoPlay/VideoPlay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Youtube from 'react-youtube';
+
+const VideoPlay = ({id}) => {
+    return (
+        <div>
+            <Youtube videoId={id} />
+        </div>
+    )
+}
+
+export default VideoPlay
