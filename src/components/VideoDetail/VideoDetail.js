f1dedd167bf0d02b5bda3909e3adc30f48115b16
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-18 21:36:08 +0900

    videoの詳細を取得

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,12 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { useLocation } from 'react-router-dom';
+import { fetchSelectedData } from '../../apis';
+import { Store } from '../../store/index';
 
 const VideoDetail = () => {
     const location = useLocation();
-    useEffect(() => {
+    const { globalState, setGlobalState } = useContext(Store);
+    const setSelectedVideo = async () => {
         const searchParams = new URLSearchParams(location.search);
         const id = searchParams.get('v');
         console.log('id', id);
+        await fetchSelectedData(id).then((res) => {
+            console.log(res);
+            const item = res.data.items.shift();
+            setGlobalState({ type: 'SET_SELECTED', payload: {selected: item} });
+        });
+    };
+    useEffect(() => {
+        setSelectedVideo();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
     return (
