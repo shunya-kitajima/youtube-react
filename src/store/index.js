bd1685739374600891fb34e30495aa4d4e60273b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-21 21:35:11 +0900

    関連動画の取得処理を追加

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import React, { createContext, useReducer } from 'react';
 
 const initialState = {
     popular: [],
+    related: [],
     selected: {}
 };
 
@@ -9,8 +10,10 @@ const reducer = (state, action) => {
     switch(action.type) {
         case 'SET_POPULAR':
             return { popular: action.payload.popular };
+        case 'SET_RELATED':
+            return { popular: action.payload.related };
         case 'SET_SELECTED':
-            return { ...state, selected: action.payload.selected };
+            return { ...state, selected: action.payload.selected };  
         default:
             return state;
     };
