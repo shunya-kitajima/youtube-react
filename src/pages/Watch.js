fa186c62d6016a5de09405ac62c56108063333e5
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-23 17:18:26 +0900

    関連動画取得の処理を修正

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect, useContext } from 'react'
 import Layout from '../components/layout/Layout';
 import SideList from '../components/SideList/SideList';
 import VideoDetail from '../components/VideoDetail/VideoDetail';
+import { Store } from '../store/index';
+import { useLocation } from 'react-router-dom';
+import { fetchSelectedData, fetchRelatedData } from '../apis/index';
 
 const Watch = () => {
+    const { setGlobalSate } = useContext(Store);
+    const location = useLocation();
+    const SetVideos = async () => {
+        const searchParams = new URLSearchParams(location.search);
+        const id = searchParams.get('v');
+        if (id) {
+            const [selected, related] = await Promise.all([fetchSelectedData(id), fetchRelatedData(id)]);
+            setGlobalSate({type: 'SET_SELECTED', payload: {selected: selected.data.items.shift()}});
+            setGlobalSate({type: 'SET_RELATED', payload: {related: related.data.items}});
+        }
+    };
+    useEffect(() => {
+        SetVideos();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [location.search]);
     return (
         <Layout>
             <VideoDetail />
