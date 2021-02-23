2221623bbcc22518c99bddaaa4a60f129247a32b
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-23 17:18:41 +0900

    関連動画取得の処理を削除

--- src/components/VideoDetail/VideoDetail.js
+++ src/components/VideoDetail/VideoDetail.js
@@ -1,28 +1,12 @@
-import React, { useEffect, useContext } from 'react';
-import { useLocation } from 'react-router-dom';
-import { fetchSelectedData } from '../../apis';
+import React, { useContext } from 'react';
 import { Store } from '../../store/index';
 import VideoPlay from '../VideoPlay/VideoPlay';
 import Style from './VideoDetail.module.scss';
 import Linkify from 'react-linkify';
 
 const VideoDetail = () => {
-    const location = useLocation();
-    const { globalState, setGlobalState } = useContext(Store);
-    const setSelectedVideo = async () => {
-        const searchParams = new URLSearchParams(location.search);
-        const id = searchParams.get('v');
-        console.log('id', id);
-        await fetchSelectedData(id).then((res) => {
-            console.log(res);
-            const item = res.data.items.shift();
-            setGlobalState({ type: 'SET_SELECTED', payload: {selected: item} });
-        });
-    };
-    useEffect(() => {
-        setSelectedVideo();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [location.search])
+    const { globalState } = useContext(Store);
+    
     return globalState.selected && globalState.selected.id ? (
         <div className={Style.wrap}>
             <VideoPlay id={globalState.selected.id} />
