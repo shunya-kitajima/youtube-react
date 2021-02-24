73f30be2dd3329676098d36bcee407ea13d1a125
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-24 23:04:30 +0900

    修正

--- src/pages/Watch.js
+++ src/pages/Watch.js
@@ -7,15 +7,15 @@ import { useLocation } from 'react-router-dom';
 import { fetchSelectedData, fetchRelatedData } from '../apis/index';
 
 const Watch = () => {
-    const { setGlobalSate } = useContext(Store);
+    const { setGlobalState } = useContext(Store);
     const location = useLocation();
     const setVideos = async () => {
         const searchParams = new URLSearchParams(location.search);
         const id = searchParams.get('v');
         if (id) {
             const [selected, related] = await Promise.all([fetchSelectedData(id), fetchRelatedData(id)]);
-            setGlobalSate({type: 'SET_SELECTED', payload: {selected: selected.data.items.shift()}});
-            setGlobalSate({type: 'SET_RELATED', payload: {related: related.data.items}});
+            setGlobalState({type: 'SET_SELECTED', payload: {selected: selected.data.items.shift()}});
+            setGlobalState({type: 'SET_RELATED', payload: {related: related.data.items}});
         }
     };
     useEffect(() => {
