f1f32d038bb5d3e4b9570a8b0ea70cb0ccf1c694
shunya-kitajima <42736897+shunya-kitajima@users.noreply.github.com>
2021-02-27 14:53:08 +0900

    検索実行時に検索結果をglobalStateに保存するよう修正

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import Layout from '../components/layout/Layout';
+import { useLocation } from 'react-router-dom';
+import { fetchSearchData } from '../apis';
+import { Store } from '../store/index';
 
 const Search = () => {
+    const { globalState, setGlobalState} = useContext(Store);
+    const location = useLocation();
+    const setSearchResult = async () => {
+        const searchParams = new URLSearchParams(location.search);
+        const query = searchParams.get('query');
+        if (query) {
+            await fetchSearchData(query).then((resp) => {
+                setGlobalState({type: 'SET_SEARCHED', payload: {searched: resp.data.items}});
+            });
+        }
+    };
+    useEffect(() => {
+        setSearchResult();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     return (
         <Layout>
             Search page
